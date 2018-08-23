953eccdd501706ede0b4cc113a5c88b360704b5c
blnicholson <blnicholson13@gmail.com>
2018-08-22 21:24:32 -0500

    almost finished product

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var fs = require("fs");
 var request=require("request");
 var moment = require("moment");
 var keys=require("./keys.js");
@@ -38,10 +39,25 @@ var input="";
     else {
       movieSearch("The Burbs");
       console.log("If you haven't watched 'The Burbs', then you should: https://www.imdb.com/title/tt0096734/");
-      console.log("It's on Netflix!");
+      console.log("You can find it on Amazon!");
     }
-        break;    
+        break;  
+    
+    case "do-what-it-says":
+    doAThing();
+    break;
  }
+function doAThing(){
+  fs.readFile("random.txt","utf8", function(error, data){
+    if(error){
+      return console.log(error)
+    }
+    var readTxt = data.split(",");
+    musicSearch(readTxt[1]);
+    
+    
+  })
+}
 
 //=========Bands in Town======//
 function bandSearch(){
@@ -56,6 +72,13 @@ function bandSearch(){
     console.log("Location: " + JSON.stringify(events[0].formatted_location,null,2))
      var date = moment(events[0].datetime).format("MM-DD-YYYY");
     console.log("Date: " + date);
+    fs.appendFileSync("log.txt","----------Concert Information----------")
+    fs.appendFileSync("log.txt","\nArtist Name: " + input);
+    fs.appendFileSync("log.txt","\nVenue: " + JSON.stringify(events[0].venue.place))
+    fs.appendFileSync("log.txt","\nLocation: " + JSON.stringify(events[0].formatted_location))
+    fs.appendFileSync("log.txt","\nDate: " + date);
+    fs.appendFileSync("log.txt"," \n ");
+    fs.appendFileSync("log.txt"," \n ");
   });
   }
 //=========Spotify========//
@@ -73,6 +96,14 @@ spotify.search({ type: 'track', query: input }, function(err,data) {
     console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url));
     //Album
     console.log("Album Name: " + JSON.stringify(data.tracks.items[0].album.name))
+
+    fs.appendFileSync("log.txt","----------Spotify Search Results----------")
+    fs.appendFileSync("log.txt","\nArtist Name: " + JSON.stringify(data.tracks.items[0].artists[0].name));
+    fs.appendFileSync("log.txt","\nSong Name: " + JSON.stringify(data.tracks.items[0].name));
+    fs.appendFileSync("log.txt","\nPreview URL: " + JSON.stringify(data.tracks.items[0].preview_url));
+    fs.appendFileSync("log.txt","\nAlbum Name: " + JSON.stringify(data.tracks.items[0].album.name))
+    fs.appendFileSync("log.txt"," \n ");
+    fs.appendFileSync("log.txt"," \n ");
   });
 }
 //==========OMDB========//
@@ -88,6 +119,18 @@ function movieSearch(input){
       console.log("Language: " + JSON.parse(body).Language);
       console.log("Plot: " + JSON.parse(body).Plot);
       console.log("Actors: " + JSON.parse(body).Actors);
+
+      fs.appendFileSync("log.txt","----------Movie Search Information----------")
+      fs.appendFileSync("log.txt","\nTitle: " + JSON.parse(body).Title);
+      fs.appendFileSync("log.txt","\nYear: "+ JSON.parse(body).Year);
+      fs.appendFileSync("log.txt","\nIMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+      fs.appendFileSync("log.txt","\nRotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+      fs.appendFileSync("log.txt","\nCountry: " + JSON.parse(body).Country);
+      fs.appendFileSync("log.txt","\nLanguage: " + JSON.parse(body).Language);
+      fs.appendFileSync("log.txt","\nPlot: " + JSON.parse(body).Plot);
+      fs.appendFileSync("log.txt","\nActors: " + JSON.parse(body).Actors);
+      fs.appendFileSync("log.txt"," \n ");
+      fs.appendFileSync("log.txt"," \n ");
     }
   })
   }
